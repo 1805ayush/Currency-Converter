a466145d673e20cbff8c9cf9463c951ccfb9abc0
Ayush <ayush.chakladar@gmail.com>
2021-07-25 18:12:47 +0530

    changed to exchange rate api

--- lib/converter.dart
+++ lib/converter.dart
@@ -1,29 +1,28 @@
 import 'dart:convert';
-
-import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 
 class Converter{
 
-  String baseURL= 'https://api.currencylayer.com/convert?access_key=';
-  String apiKey = 'e8831454ea77b7fed61dd24340774b01';
+  String baseURL= 'https://v6.exchangerate-api.com/v6/';
+  String apiKey = '286d9e5370b2cb3ccb043da0';
   String fromCurrency;
   String toCurrency;
   String amount;
+  //https://v6.exchangerate-api.com/v6/YOUR-API-KEY/pair/EUR/GBP/AMOUNT
 
   Future convertCurrency(fromCurrency,toCurrency,amount) async{
 
-    String requestURL="$baseURL$apiKey&from=$fromCurrency&to=$toCurrency&amount=$amount";
+    String requestURL="$baseURL$apiKey/pair/$fromCurrency/$toCurrency/$amount";
 
     http.Response response= await http.get(requestURL);
     if(response.statusCode==200){
       var decodedData = jsonDecode(response.body);
       print(decodedData);
-      double price = decodedData['result'];
+      double price = decodedData['conversion_result'];
       print(price);
 
     }else{
-
+      print(response.statusCode);
     }
 
 
